189bac4d5172c7f7578525ef2d57a137ec433f87
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-04-28 16:14:37 +0200

    Add delete and clear commands to multiclipboard

--- 08_multiclipboard/mcb.rb
+++ 08_multiclipboard/mcb.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby -w
+#!/usr/bin/env ruby
 
 require 'clipboard'
 require 'yaml'
@@ -29,6 +29,18 @@ when 'list'
   return unless File.exist?(STORAGE_PATH)
   storage = YAML.load_file(STORAGE_PATH)
   storage.each { |key, value| puts "#{key}: #{value}" }
+when 'delete'
+  if key
+    return unless File.exist?(STORAGE_PATH)
+    storage = YAML.load_file(STORAGE_PATH)
+    storage.delete(key)
+    File.open(STORAGE_PATH, 'w') { |f| YAML.dump(storage, f) }
+  else
+    STDERR.puts 'mcb: key not specified'
+  end
+when 'clear'
+  return unless File.exist?(STORAGE_PATH)
+  File.delete(STORAGE_PATH)
 else
   STDERR.puts "mcb: command #{command} not supported"
 end
